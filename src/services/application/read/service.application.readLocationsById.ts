318c08d2a90e9fd8a43380a68c196d79aa03dd7e
phanthang-creat <phanthang052@gmail.com>
2023-02-27 14:49:58 +0700

    fix read application id bug

--- src/services/application/read/service.application.readLocationsById.ts
+++ src/services/application/read/service.application.readLocationsById.ts
@@ -4,12 +4,16 @@ import { executeQuery } from "../../../configs/database";
 const readApplicationLocationsByIdService = async (applicationId: number) => {
     try {
         logging.info("Read application locations service start ...");
-        const query = "SELECT applications_locations.location_id as district_id, districts.name as district," +
-        " districts.province_id as province_id, provinces.name as provinces" +
-        " FROM applications_locations" +
-        " JOIN districts ON applications_locations.location_id = districts.id" +
-        " JOIN provinces ON districts.province_id = provinces.id" +
-        " WHERE applications_locations.application_id = ?";
+        const query = "SELECT applications_locations.location_id as district_id, " +
+        "districts.full_name as district," + 
+        "districts.name as district_name," +
+        "provinces.full_name as province," +
+        "provinces.name as province_name," +
+        "provinces.id as province_id " +
+        "FROM applications_locations " +
+        "JOIN districts ON applications_locations.location_id = districts.id " +
+        "JOIN provinces ON districts.province_id = provinces.id " +
+        "WHERE applications_locations.application_id = ?";
         const params = [applicationId];
         const res = await executeQuery(query, params);
         // console.log(res);
