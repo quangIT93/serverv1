5d2a442d9662a986b0a25980cbb00fb23b5a3784
phanthang-creat <phanthang052@gmail.com>
2023-03-20 09:07:26 +0700

    fix chat and fb login

--- src/services/aws/service.aws.uploadImages.ts
+++ src/services/aws/service.aws.uploadImages.ts
@@ -2,15 +2,12 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { v4 as uuidv4 } from "uuid";
 import logging from "../../utils/logging";
 
-const uploadImages = async (
-        files, 
-        postsId: number | null = null
-) => {
+const uploadImages = async (files) => {
     try {
-        logging.info("Upload files  to aws service start ...");
+        logging.info("Upload files to aws service start ...");
         const client = new S3Client({ region: process.env.AWS_REGION });
         const params = files.map((file) => ({
-            Bucket: process.env.AWS_BUCKET_NAME + (postsId ? `/posts-image/${postsId}` : ""),
+            Bucket: process.env.AWS_BUCKET_NAME,
             Key: `${Date.now()}-${uuidv4()}.jpg`,
             Body: file.buffer,
         }));
@@ -24,7 +21,7 @@ const uploadImages = async (
                 `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${param.Key}`
         );
     } catch (error) {
-        logging.error(`Upload files to aws service error: ${error}`);
+        logging.error(`Upload files to aws servie error: ${error}`);
         throw error;
     }
 };
