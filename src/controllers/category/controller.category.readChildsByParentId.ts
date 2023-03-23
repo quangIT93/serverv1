6c8124b3fdfc204161d5279b5c094563ce413f6c
phanthang-creat <phanthang052@gmail.com>
2023-03-23 10:02:01 +0700

    Update controller.category.readChildsByParentId.ts

--- src/controllers/category/controller.category.readChildsByParentId.ts
+++ src/controllers/category/controller.category.readChildsByParentId.ts
@@ -4,6 +4,7 @@ import { Request, Response, NextFunction } from "express";
 import logging from "../../utils/logging";
 import * as categoryServices from "../../services/category/_service.category";
 import { sortChildrenCategory } from "./_sortChildrenCategory";
+import { lang } from "moment";
 
 const readChildCategoriesByParentCategoryId = async (
     req: Request,
@@ -15,6 +16,12 @@ const readChildCategoriesByParentCategoryId = async (
             "Read child categories by parent category id controller start ..."
         );
 
+        const { lang = "vi" } = req.query;
+
+        if (lang !== "vi" && lang !== "en" && lang !== "ko") {
+            return next(createError(400, "Invalid lang"));
+        }
+        
         // GET PARENT CATEGORY ID
         const parentCategoryId = +req.query.pid;
 
@@ -26,7 +33,8 @@ const readChildCategoriesByParentCategoryId = async (
         // GET CHILD CATEGORIES
         const childCategories =
             await categoryServices.readChildCategoriesByParentCategoryId(
-                parentCategoryId
+                parentCategoryId,
+                lang
             );
         if (!childCategories) {
             return next(createError(500));
