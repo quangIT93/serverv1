579df1dbc0ecd6911aed3fcd1e90d739dd147581
Loops <phanthang052@gmail.com>
2023-03-28 17:24:21 +0700

    Update service.post.readAllPostsByAdmin.ts

--- src/services/post/service.post.readAllPostsByAdmin.ts
+++ src/services/post/service.post.readAllPostsByAdmin.ts
@@ -5,11 +5,31 @@ const readAllPostsByAdmin = async () => {
     try {
         logging.info("Read posts service start ...");
         const query =
-            "SELECT posts.id, posts.status, posts.account_id, posts.title, posts.company_name, " +
-            "posts.created_at, profiles.name as poster " +
+            "SELECT posts.id, " +
+            "posts.status, " +
+            "posts.account_id, " +
+            "posts.title, " +
+            "posts.company_name, " +
+            "posts.created_at, " +
+            "profiles.name as poster, " +
+            "posts.ward_id, " +
+            "wards.full_name as ward," + 
+            "wards.name as ward_name," +
+            "districts.id as district_id," +
+            "districts.full_name as district," + 
+            "districts.name as district_name," +
+            "provinces.full_name as province," +
+            "provinces.name as province_name," +
+            "provinces.id as province_id " +
             "FROM posts " +
             "LEFT JOIN profiles " +
             "ON profiles.id = posts.account_id " +
+            "LEFT JOIN wards " +
+            "ON wards.id = posts.ward_id " +
+            "LEFT JOIN districts " +
+            "ON districts.id = wards.district_id " +
+            "LEFT JOIN provinces " +
+            "ON provinces.id = districts.province_id " +
             "GROUP BY posts.id " +
             "ORDER BY posts.id DESC";
         const res = await executeQuery(query);
