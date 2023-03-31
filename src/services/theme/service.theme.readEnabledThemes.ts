481eff275ba3553ec869db62863585ffeea763ee
Loops <phanthang052@gmail.com>
2023-03-31 22:34:36 +0700

    Update service.theme.readEnabledThemes.ts

--- src/services/theme/service.theme.readEnabledThemes.ts
+++ src/services/theme/service.theme.readEnabledThemes.ts
@@ -6,26 +6,46 @@ const readEnabledThemes = async (
 ) => {
     try {
         logging.info("Read enabled themes service start ...");
-        let query = 
+        // let query = 
+        //     "SELECT themes.id, " +
+        //     "themes.title, " +
+        //     "themes.image, " +
+        //     "themes.district_id, " +
+        //     "COUNT(themes_posts.post_id) AS number_of_posts " +
+        //     "FROM themes " +
+        //     "LEFT JOIN themes_posts " +
+        //     "ON themes_posts.theme_id = themes.id " +
+        //     "LEFT JOIN posts " +
+        //     "ON posts.id = themes_posts.post_id " +
+        //     "LEFT JOIN districts " +
+        //     "ON districts.id = themes.district_id " +
+        //     "WHERE themes.status = ? AND posts.status = 1 " +
+        //     `${provinces.length > 0 ? "AND districts.province_id IN " + 
+        //     "(" + provinces.map((_) => "?").join(", ") + ")" : ""}` +
+        //     "GROUP BY themes.id";
+        // let params = [1, ...provinces];
+
+        // Each theme I want to get all posts have district_id = themes.district_id 
+        // But post just have ward_id, so I need to get district_id from ward_id
+        let query =
             "SELECT themes.id, " +
             "themes.title, " +
             "themes.image, " +
             "themes.district_id, " +
-            "COUNT(themes_posts.post_id) AS number_of_posts " +
+            "COUNT(posts.id) AS number_of_posts " +
             "FROM themes " +
-            "LEFT JOIN themes_posts " +
-            "ON themes_posts.theme_id = themes.id " +
-            "LEFT JOIN posts " +
-            "ON posts.id = themes_posts.post_id " +
+            "LEFT JOIN wards " +
+            "ON wards.district_id = themes.district_id " +
             "LEFT JOIN districts " +
-            "ON districts.id = themes.district_id " +
+            "ON districts.id = wards.district_id " +
+            "LEFT JOIN posts " +
+            "ON posts.ward_id = wards.id " +
             "WHERE themes.status = ? AND posts.status = 1 " +
-            `${provinces.length > 0 ? "AND districts.province_id IN " + 
+            `${provinces.length > 0 ? "AND districts.province_id IN " +
             "(" + provinces.map((_) => "?").join(", ") + ")" : ""}` +
             "GROUP BY themes.id";
-        let params = [1, ...provinces];
-
         // console.log(query)
+        let params = [1, ...provinces];
 
         const res = await executeQuery(query, params);
         return res ? res : null;
