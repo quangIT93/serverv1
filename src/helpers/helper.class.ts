b865760808214b7354e73b9485bb1930a424c508
phanthang-creat <phanthang052@gmail.com>
2023-04-03 14:56:33 +0700

    Update helper.class.ts

--- src/helpers/helper.class.ts
+++ src/helpers/helper.class.ts
@@ -10,7 +10,7 @@ class Helper {
   }
 
   checkPhoneNumberFormat(phoneNumber: string): boolean {
-    const phoneNumberRegexWith0 = new RegExp(/^(0[3|5|7|8|9])+([0-9]{8,9})\b$/)
+    const phoneNumberRegexWith0 = new RegExp(/^(0[2|3|5|7|8|9])+([0-9]{8,9})\b$/)
     const phoneNumberRegexWith84 = new RegExp(/^(84)+([0-9]{9,11})\b$/)
     return phoneNumberRegexWith0.test(phoneNumber) || phoneNumberRegexWith84.test(phoneNumber);
   }
