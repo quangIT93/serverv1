0ff579df1d85619a2572f259732e2071caa2536a
Phan Công Thăng <57101433+phanthang-creat@users.noreply.github.com>
2023-07-04 10:17:21 +0700

    fix xss

--- src/controllers/notification/createNotificationContent/application/generateBodyHTML.ts
+++ src/controllers/notification/createNotificationContent/application/generateBodyHTML.ts
@@ -1,3 +1,5 @@
+import validator from "validator";
+
 const generateBodyHTML = ({
     type,
     applicationStatus,
@@ -13,43 +15,47 @@ const generateBodyHTML = ({
     name: string;
     lang?: string;
 }) => {
+    let postTitleHTML = validator.escape(postTitle);
+    let companyNameHTML = validator.escape(companyName);
+    let nameHTML = validator.escape(name);
+
     switch (type) {
         case 0:
             switch (applicationStatus) {
                 case 2:
                     switch (lang) {
                         case "vi":
-                            return `Đơn ứng tuyển vị trí <b style="color: #0D99FF;">${postTitle}</b> cho ${companyName} của bạn đã được nhà tuyển dụng duyệt!`;
+                            return `Đơn ứng tuyển vị trí <b style="color: #0D99FF;">${postTitleHTML}</b> cho ${companyNameHTML} của bạn đã được nhà tuyển dụng duyệt!`;
                         case "en":
-                            return `Your application for the position of <b style="color: #0D99FF;">${postTitle}</b> for ${companyName} has been approved`;
+                            return `Your application for the position of <b style="color: #0D99FF;">${postTitleHTML}</b> for ${companyNameHTML} has been approved`;
                         case "ko":
-                            return `당신의 ${companyName} 사의 <b style="color: #0D99FF;">${postTitle}</b> 포지션 지원서가 고용주에 의해 승인되었습니다!`;
+                            return `당신의 ${companyNameHTML} 사의 <b style="color: #0D99FF;">${postTitleHTML}</b> 포지션 지원서가 고용주에 의해 승인되었습니다!`;
                         default:
-                            return `Đơn ứng tuyển vị trí <b style="color: #0D99FF;">${postTitle}</b> cho ${companyName} của bạn đã được nhà tuyển dụng duyệt!`;
+                            return `Đơn ứng tuyển vị trí <b style="color: #0D99FF;">${postTitleHTML}</b> cho ${companyNameHTML} của bạn đã được nhà tuyển dụng duyệt!`;
                     }
                 case 4:
                     switch (lang) {
                         case "vi":
-                            return `Chúc mừng bạn! Bạn đã được nhà tuyển dụng xác nhận tuyển cho vị trí <b style="color: #0D99FF;">${postTitle}</b> cho ${companyName}.`;
+                            return `Chúc mừng bạn! Bạn đã được nhà tuyển dụng xác nhận tuyển cho vị trí <b style="color: #0D99FF;">${postTitleHTML}</b> cho ${companyNameHTML}.`;
                         case "en":
-                            return `Congratulation! You have been confirmed by the employer for the position of <b style="color: #0D99FF;">${postTitle}</b> for ${companyName}.`
+                            return `Congratulation! You have been confirmed by the employer for the position of <b style="color: #0D99FF;">${postTitleHTML}</b> for ${companyNameHTML}.`
                         case "ko":
-                            return `축하드립니다 ${companyName} 에서 <b style="color: #0D99FF;">${postTitle}</b> 로 채용이 확정되었습니다!`;
+                            return `축하드립니다 ${companyNameHTML} 에서 <b style="color: #0D99FF;">${postTitleHTML}</b> 로 채용이 확정되었습니다!`;
                         default:
-                            return `Chúc mừng bạn! Bạn đã được nhà tuyển dụng xác nhận tuyển cho vị trí <b style="color: #0D99FF;">${postTitle}</b> cho ${companyName}.`;
+                            return `Chúc mừng bạn! Bạn đã được nhà tuyển dụng xác nhận tuyển cho vị trí <b style="color: #0D99FF;">${postTitleHTML}</b> cho ${companyNameHTML}.`;
                     }
             }
             break;
         case 1:
             switch (lang) {
                 case "vi":
-                    return `Ứng viên ${name} vừa nộp hồ sơ ứng tuyển vị trí <b style="color: #0D99FF;">${postTitle}</b> cho ${companyName} của bạn.`
+                    return `Ứng viên ${nameHTML} vừa nộp hồ sơ ứng tuyển vị trí <b style="color: #0D99FF;">${postTitleHTML}</b> cho ${companyNameHTML} của bạn.`
                 case "en":
-                    return `Candidate ${name} has just applied for the position of <b style="color: #0D99FF;">${postTitle}</b> for your ${companyName}.`
+                    return `Candidate ${nameHTML} has just applied for the position of <b style="color: #0D99FF;">${postTitleHTML}</b> for your ${companyNameHTML}.`
                 case "ko":
-                    return `${name} 후보자는 방금 당신의 ${companyName} 회사에 <b style="color: #0D99FF;">${postTitle}</b> 포지션 지원서를 제출했습니다.`;
+                    return `${nameHTML} 후보자는 방금 당신의 ${companyNameHTML} 회사에 <b style="color: #0D99FF;">${postTitleHTML}</b> 포지션 지원서를 제출했습니다.`;
                 default:
-                    return `Ứng viên ${name} vừa nộp hồ sơ ứng tuyển vị trí <b style="color: #0D99FF;">${postTitle}</b> cho ${companyName} của bạn.`
+                    return `Ứng viên ${nameHTML} vừa nộp hồ sơ ứng tuyển vị trí <b style="color: #0D99FF;">${postTitleHTML}</b> cho ${companyNameHTML} của bạn.`
             }
     }
 };
