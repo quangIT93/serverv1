b3d96cca1deab9b357cb40a54f240021080964cc
Phan Công Thăng <57101433+phanthang-creat@users.noreply.github.com>
2023-11-03 11:46:50 +0700

    add gzip for location api

--- src/routes/app/v1/route.app.location.ts
+++ src/routes/app/v1/route.app.location.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import locationController from "../../../controllers/location/_controller.location";
+import compression from "compression";
 
 const router = express.Router();
-
+router.use(compression());
 router.get("/p", locationController.readAllProvinces);
 router.get("/d", locationController.readDistrictsByProvince);
 router.get("/", locationController.readAllLocations);
-router.get("/w", locationController.readWardsByDistrict);
+router.get("/w", compression(), locationController.readWardsByDistrict);
 
 export default router;
